06e2c63b31d4b9049060e67357fcdca7daf29594
FANOR1945 <aguirrefanor31@gmail.com>
2022-06-04 06:41:21 +0000

    Delete category api Giphy

--- src/GifMainApp.js
+++ src/GifMainApp.js
@@ -3,6 +3,10 @@ import { AddCategory } from './components/AddCategory';
 import { GifGrid } from './components/GifGrid' ;
 export const GifMainApp = () => {
   const [categories, setCategories] = useState(['The Simpsons', 'Rick y Morty']);
+  const removeCategory = (id) => {
+    const newCategories = categories.filter(item => item != id);
+    setCategories(newCategories);
+  };
   return (
     <>
       <h2>Memes</h2>
@@ -10,7 +14,7 @@ export const GifMainApp = () => {
       <ol>
         {categories.map((category) => (
          
-          <GifGrid category={category} />
+          <GifGrid category={category} removeCategory={removeCategory}/>
   ))}
       </ol>
       
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -2,18 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useFetch } from "../hooks/useFetch";
 import { GifGridItem } from "./GifGridItem";
-export const GifGrid = ({ category }) => {
+export const GifGrid = ({ category, removeCategory }) => {
   const { data, loading } = useFetch(category);
   return (
     <ol>
-      <div className='cardContainer'>
+      <div className="cardContainer">
       <div className="card-grid">
       
             {data.map(img => (
             <GifGridItem key={img.id} { ...img}/>
                 ))}
                 
-            <div className="close">Close</div>
+            <div className="close" onClick={() => {removeCategory(category)}}>Close</div>
             </div>
       </div>
     </ol>
